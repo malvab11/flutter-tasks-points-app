ceb3c0c5d1190f4c5e9c80c0f186da48fbc3af3b
Marlon Alva <marlonalvab@hotmail.com>
2025-07-04 00:32:03 -0500

    App de Misison Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mission_up/features/loading/screens/loading_screen.dart';
-import 'package:mission_up/features/login/screens/login_screen.dart';
-import 'package:mission_up/features/presentation/screens/presentation_screen.dart';
-import 'package:mission_up/features/register_tutor/screens/register_tutor_screen.dart';
-import 'package:mission_up/features/register_user/screens/register_user_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,17 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mission Up',
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'loading',
-      routes: {
-        'loading': (context) => const LoadingScreen(),
-        'presentation': (context) => const PresentationScreen(),
-        'login': (context) => const LoginScreen(),
-        'registerTutor': (context) => const RegisterTutorScreen(),
-        'registerUser': (context) => const RegisterUserScreen(),
-      },
-    );
+    return MaterialApp(title: 'Mission Up', debugShowCheckedModeBanner: false);
   }
 }
